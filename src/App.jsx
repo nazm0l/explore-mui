48dcbaf547a9b89f503d43a30ffd825a6d779b21
nazm0l <tanjirahmednazmul@gmail.com>
2023-03-14 23:12:47 +0600

    Setup React and Material UI

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@mui/material";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Click Me!</Button>
+    </div>
+  );
+}
+
+export default App;
