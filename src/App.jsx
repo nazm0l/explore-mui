b3feb2570b24682398480b62282e64064e7a86ea
nazm0l <tanjirahmednazmul@gmail.com>
2023-03-16 15:56:52 +0600

    Explore Card Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { Button, CssBaseline } from "@mui/material";
 import "./App.css";
+import Card from "./components/ProfileCard";
 import EpBox from "./components/EpBox";
 
 function App() {
   return (
     <div className="App">
       <EpBox />
+      <Card />
+
+
       <CssBaseline />
     </div>
   );
---
+++ src/components/ProfileCard.jsx
@@ -0,0 +1,36 @@
+import { Box } from "@mui/material";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+const ProfileCard = () => {
+  return (
+    <Box>
+      <Card sx={{ maxWidth: 345, mt:2 }}>
+        <CardMedia
+          sx={{ height: 240, objectFit: "cover", bgcolor:'red' }}
+          image="https://futhead.cursecdn.com/static/img/21/players_alt/p134375751.png"
+          title="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Share</Button>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+    </Box>
+  );
+};
+
+export default ProfileCard;
